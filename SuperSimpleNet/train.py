674dac1ac14e04f6abf50d8cb2c7f36905ec3ca4
musaemiraktas <aktasmusaemir@gmail.com>
2025-06-10 04:58:32 +0300

    unsup

--- SuperSimpleNet/train.py
+++ SuperSimpleNet/train.py
@@ -830,8 +830,8 @@ def run_sup(data_name):
 
 
 def main():
-    #run_unsup(sys.argv[1])
-    run_sup(sys.argv[1])
+    run_unsup(sys.argv[1])
+    #run_sup(sys.argv[1])
 
 
 if __name__ == "__main__":
